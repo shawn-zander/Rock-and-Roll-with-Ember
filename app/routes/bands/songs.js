4469372933df78bb7e256fadc5970d61a583a7be
Shawn Zander <shawn.zander@coschedule.com>
2018-04-25 21:13:09 -0500

    Finished Ch. 5

---
+++ app/routes/bands/songs.js
@@ -0,0 +1,8 @@
+// app/routes/bands/band/songs.js
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    model: function () {
+        return this.modelFor('bands.band');
+    }
+});
